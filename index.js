9e938552a5ec99920ec32204d12c19e71053c1c1
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-05-11 15:07:59 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
